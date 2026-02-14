e069efd17670cb666734992cc9ef0a4be03c2c1c
amanhnb88 <aldry.manu02@gmail.com>
2026-02-15 06:35:39 +0900

    Fix formatting in JavHeyPlugin class

--- JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
+++ JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
@@ -4,9 +4,9 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import android.content.Context
 
 @CloudstreamPlugin
-class JavHeyPlugin: CloudstreamPlugin() {
+class JavHeyPlugin : CloudstreamPlugin() {
     override fun load(context: Context) {
-        // Mendaftarkan class utama JavHey yang kode-nya sudah kamu berikan sebelumnya
+        // Mendaftarkan provider JavHey
         registerMainAPI(JavHey())
     }
 }
