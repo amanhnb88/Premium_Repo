bdfddd0a7b1b45dbc8c39e4c8cfdac6f85dc4837
amanhnb88 <aldry.manu02@gmail.com>
2026-02-15 07:17:13 +0900

    Update JavHeyPlugin.kt

--- JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
+++ JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
@@ -1,10 +1,11 @@
-package com.JavHey
+package com.javhey
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class JavHeyPlugin : CloudstreamPlugin() {
+class JavHeyPlugin: Plugin() {
     override fun load(context: Context) {
         // Mendaftarkan provider JavHey
         registerMainAPI(JavHey())
