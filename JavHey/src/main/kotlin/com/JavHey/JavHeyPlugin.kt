a67309c65b3c11617ac07a05f0062bee5a20fbd1
amanhnb88 <aldry.manu02@gmail.com>
2026-02-15 10:17:56 +0900

    Update JavHeyPlugin.kt

--- JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
+++ JavHey/src/main/kotlin/com/JavHey/JavHeyPlugin.kt
@@ -1,18 +1,13 @@
 package com.javhey
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class JavHeyPlugin: Plugin() {
+class JavHeyPlugin : CloudstreamPlugin() {
     override fun load(context: Context) {
-        // Daftarkan Provider V4
-        registerMainAPI(JavHeyV4())
-        
-        // Daftarkan Extractor Lokal yang ada di file JavHey.kt
-        registerExtractorAPI(ByseBuhoLocal())
-        registerExtractorAPI(BysezejataosLocal())
-        registerExtractorAPI(ByseVepoinLocal())
+        // Kita hanya perlu mendaftarkan MainAPI saja.
+        // Ekstraktor Byse sudah dipanggil secara manual di dalam JavHey.kt
+        registerMainAPI(JavHey())
     }
 }
