879b38f228e486eb2c393c6d3f625bbc01263c74
amanhnb88 <aldry.manu02@gmail.com>
2026-02-16 14:14:10 +0900

    Update NgeFilmProviderPlugin.kt

--- NgeFilm/src/main/kotlin/com/michat88/NgeFilmProviderPlugin.kt
+++ NgeFilm/src/main/kotlin/com/michat88/NgeFilmProviderPlugin.kt
@@ -5,14 +5,8 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class NgeFilmProviderPlugin : Plugin() {
+class NgefilmPlugin: Plugin() {
     override fun load(context: Context) {
-        // Register main provider
         registerMainAPI(NgefilmProvider())
-        
-        // Register custom extractors
-        registerExtractorAPI(NgefilmExtractor())
-        registerExtractorAPI(NgefilmGdriveExtractor())
-        registerExtractorAPI(NgefilmEmbedExtractor())
     }
 }
