3ddab0229444d5eba60a164513c70753d1d9c12e
amanhnb88 <aldry.manu02@gmail.com>
2026-02-18 06:07:26 +0900

    Create AdiFilmSemiPlugin.kt

---
+++ AdiFilmSemi/src/main/kotlin/com/michat88/AdiFilmSemiPlugin.kt
@@ -0,0 +1,17 @@
+package com.michat88
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class AdiFilmSemiPlugin : Plugin() {
+    override fun load(context: Context) {
+        // Register Main Provider
+        registerMainAPI(AdiFilmSemi())
+        
+        // Register Extractors
+        // Mendaftarkan Jeniusplay yang baru (dari IdlixProvider)
+        registerExtractorAPI(Jeniusplay())
+    }
+}
