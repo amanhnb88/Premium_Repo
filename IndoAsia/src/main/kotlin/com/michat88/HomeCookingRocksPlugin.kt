bcff795e0ba0eb6b85db7bff60e3e4fda51c474d
amanhnb88 <aldry.manu02@gmail.com>
2026-02-21 16:42:14 +0900

    Update HomeCookingRocksPlugin.kt

--- IndoAsia/src/main/kotlin/com/michat88/HomeCookingRocksPlugin.kt
+++ IndoAsia/src/main/kotlin/com/michat88/HomeCookingRocksPlugin.kt
@@ -7,10 +7,8 @@ import android.content.Context
 @CloudstreamPlugin
 class HomeCookingRocksPlugin : Plugin() {
     override fun load(context: Context) {
-        // Mendaftarkan Provider Utama (Website HomeCookingRocks)
         registerMainAPI(HomeCookingRocks())
-        
-        // Mendaftarkan Extractor Khusus untuk Server 2 (4MePlayer)
+        // WAJIB didaftarkan agar class di Extractor.kt dikenali
         registerExtractorAPI(FourMePlayerExtractor())
     }
 }
